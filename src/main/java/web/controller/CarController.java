7152efe250238c5483f13a18a7b53e6c77a0256d
Max Sheremet <leheupofall@yandex.ru>
2025-04-11 15:02:39 +0300

    inject service layer bean into controller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,21 +1,24 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping("/cars")
-    public String printCar(@RequestParam(value = "count", required = false, defaultValue = "-1") int count, ModelMap model) {
-        CarService carService = new CarServiceImpl();
+    public String printCar(@RequestParam(value = "count", required = false, defaultValue = "-1") int count,
+                           ModelMap model) {
         model.addAttribute("cars", carService.getCarsByCount(count));
         return "cars";
     }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,10 +1,13 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService {
     private List<Car> cars;
 
